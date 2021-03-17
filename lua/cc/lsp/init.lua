ffca6f16b911d4cea7582f3689f05fdc021aca72
Claudio Caseiro <ccaseiro@gmail.com>
2021-03-17 03:57:58 +0000

    yaml / kubernets language server

--- lua/cc/lsp/init.lua
+++ lua/cc/lsp/init.lua
@@ -1,1 +1,14 @@
+local lspconfig = require('lspconfig')
 require('cc/lsp.lua')
+
+lspconfig.yamlls.setup({
+  settings = {
+    yaml = {
+      schemas = {
+        kubernetes = "/*.yaml"
+      }
+    }
+  }
+  --on_attach = custom_attach
+})
+
