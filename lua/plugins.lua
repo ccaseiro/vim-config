dc890cb0d80636cae60961d3cc369958972ef473
Claudio Caseiro <ccaseiro@gmail.com>
2021-03-17 03:56:06 +0000

    fix: typo

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,7 +21,7 @@ return require('packer').startup(function()
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-compe'
 
-  use { 'nvim-treesitter/nvim-treesiter', run = ':TSUpdate' }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
   -- Telescope
   use 'nvim-lua/popup.nvim'
