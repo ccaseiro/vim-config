3af01b3de7d73206ba819dcd6708b6ed24e588b0
Claudio Caseiro <ccaseiro@gmail.com>
2021-04-21 15:08:13 +0100

    enable curorline

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,5 +1,6 @@
 vim.wo.number = true
 vim.o.hidden=true               -- required to keep multiple buffers open multiple buffers
 vim.o.clipboard = "unnamedplus"
+vim.o.cursorline = true
 vim.o.mouse = "a" 		-- enable mouse
 -- vim.o.guifont = "Dank\\ Mono:h11,Fira\\ Code:h12"
