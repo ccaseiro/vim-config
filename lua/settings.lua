74c781f0883dfcf99b3e69223edb441cfb32e481
Claudio Caseiro <ccaseiro@gmail.com>
2021-04-21 15:07:50 +0100

    enable mouse

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,4 +1,5 @@
 vim.wo.number = true
 vim.o.hidden=true               -- required to keep multiple buffers open multiple buffers
 vim.o.clipboard = "unnamedplus"
+vim.o.mouse = "a" 		-- enable mouse
 -- vim.o.guifont = "Dank\\ Mono:h11,Fira\\ Code:h12"
