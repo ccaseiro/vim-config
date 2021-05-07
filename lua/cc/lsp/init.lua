c09345f894dfc6d6102b6d0dfe65d67db4c915f1
Claudio Caseiro <ccaseiro@gmail.com>
2021-05-07 20:51:06 +0100

    better signs

--- lua/cc/lsp/init.lua
+++ lua/cc/lsp/init.lua
@@ -1,4 +1,23 @@
 local lspconfig = require('lspconfig')
+
+vim.fn.sign_define(
+    "LspDiagnosticsSignError",
+    {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"}
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignWarning",
+    {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"}
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignHint",
+    {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"}
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignInformation",
+    {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"}
+)
+
+
 require('cc/lsp.lua')
 
 lspconfig.yamlls.setup({
