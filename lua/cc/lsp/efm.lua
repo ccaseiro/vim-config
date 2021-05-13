ca118f1572f388d9152296a3a480f54d05cadf92
Claudio Caseiro <ccaseiro@gmail.com>
2021-05-13 13:16:44 +0100

    formatting: lua: column-limit=120

--- lua/cc/lsp/efm.lua
+++ lua/cc/lsp/efm.lua
@@ -13,8 +13,8 @@ require'lspconfig'.efm.setup {
       lua = {
         {
           -- formatCommand = "lua-format -i --no-keep-simple-function-one-line --no-break-after-operator --column-limit=150 --break-after-table-lb",
-          -- formatCommand = "lua-format -i --no-keep-simple-function-one-line --column-limit=120"
-          formatCommand = "lua-format -i --indent-width=2",
+          -- formatCommand = "lua-format -i --no-keep-simple-function-one-line --column-limit=120",
+          formatCommand = "lua-format -i --indent-width=2 --column-limit=120",
           formatStdin = true
         }
       }
