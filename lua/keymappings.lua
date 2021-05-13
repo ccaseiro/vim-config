673b28e3301fadcd18fc729c5b4e84ca303b0e76
Claudio Caseiro <ccaseiro@gmail.com>
2021-05-13 15:00:58 +0100

    reformatting

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -2,52 +2,51 @@ local nmap = require'cc.utils'.nmap
 local nnoremap = require'cc.utils'.nnoremap
 local wk = require("which-key")
 
-vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
 vim.g.mapleader = ' '
 
 -- no hl
-vim.api.nvim_set_keymap('n', '<Leader>ths', ':set hlsearch!<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ths', ':set hlsearch!<CR>', {noremap = true, silent = true})
 
 -- explorer
-vim.api.nvim_set_keymap('n', '<Leader>ft', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ft', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 
 -- better window movement
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {silent = true})
 
 -- better indenting
-vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
 
 -- Move selected line / block of text in visual mode
-vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
 
 -- Plugin: nvim-miniyank
-vim.api.nvim_set_keymap('', 'p', '<Plug>(miniyank-autoput)', { noremap = false, silent = true })
-vim.api.nvim_set_keymap('', 'P', '<Plug>(miniyank-autoPut)', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('', 'p', '<Plug>(miniyank-autoput)', {noremap = false, silent = true})
+vim.api.nvim_set_keymap('', 'P', '<Plug>(miniyank-autoPut)', {noremap = false, silent = true})
 
 -- TAB switch buffer
-vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprev<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprev<CR>', {noremap = true, silent = true})
 
 -- Escape
 -- vim.api.nvim_set_keymap('i', 'nn', '<ESC>', { noremap = true, silent = true })
 
 -- QuickFix
-vim.api.nvim_set_keymap('n', '<leader>q/', ':Telescope quickfix<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ql', ':copen<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qo', ':copen<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qc', ':cclose<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qp', ':cpred<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qn', ':cnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q/', ':Telescope quickfix<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>ql', ':copen<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>qo', ':copen<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>qc', ':cclose<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>qp', ':cpred<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>qn', ':cnext<CR>', {noremap = true, silent = true})
 
 -- Fils
 wk.register({["<leader>fs"] = {'<cmd>write<cr>', 'Save'}})
 wk.register({["<leader>fw"] = {'<cmd>write<cr>', 'Save'}})
-
 
 -- VimWiki
 nmap('<leader>ow', '<Plug>VimwikiIndex')
