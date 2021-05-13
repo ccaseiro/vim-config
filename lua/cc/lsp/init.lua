c67ee7cc5338431f3ac45a4e985899c68e97e0c9
Claudio Caseiro <ccaseiro@gmail.com>
2021-05-13 13:16:58 +0100

    reformatting

--- lua/cc/lsp/init.lua
+++ lua/cc/lsp/init.lua
@@ -1,31 +1,22 @@
 local lspconfig = require('lspconfig')
 local wk = require("which-key")
 
-vim.fn.sign_define(
-    "LspDiagnosticsSignError",
-    {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"}
-)
-vim.fn.sign_define(
-    "LspDiagnosticsSignWarning",
-    {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"}
-)
-vim.fn.sign_define(
-    "LspDiagnosticsSignHint",
-    {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"}
-)
-vim.fn.sign_define(
-    "LspDiagnosticsSignInformation",
-    {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"}
-)
-
+vim.fn.sign_define("LspDiagnosticsSignError",
+                   {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"})
+vim.fn.sign_define("LspDiagnosticsSignWarning",
+                   {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"})
+vim.fn.sign_define("LspDiagnosticsSignHint",
+                   {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"})
+vim.fn.sign_define("LspDiagnosticsSignInformation",
+                   {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"})
 
 local use_sagalsp = true
 
 local mappings = {
-	hover = use_sagalsp and 'Lspsaga hover_doc' or 'lua vim.lsp.buf.hover()',
-	diagnostic_prev = use_sagalsp and 'Lspsaga diagnostic_jump_prev' or 'lua vim.lsp.diagnostic.goto_prev()',
-	diagnostic_next = use_sagalsp and 'Lspsaga diagnostic_jump_next' or 'lua vim.lsp.diagnostic.goto_next()',
-	diagnostic_line = use_sagalsp and 'Lspsaga show_line_diagnostics' or 'lua vim.lsp.diagnostic.show_line_diagnostics()'
+  hover = use_sagalsp and 'Lspsaga hover_doc' or 'lua vim.lsp.buf.hover()',
+  diagnostic_prev = use_sagalsp and 'Lspsaga diagnostic_jump_prev' or 'lua vim.lsp.diagnostic.goto_prev()',
+  diagnostic_next = use_sagalsp and 'Lspsaga diagnostic_jump_next' or 'lua vim.lsp.diagnostic.goto_next()',
+  diagnostic_line = use_sagalsp and 'Lspsaga show_line_diagnostics' or 'lua vim.lsp.diagnostic.show_line_diagnostics()'
 }
 
 local mapper = function(mode, key, result)
@@ -33,27 +24,25 @@ local mapper = function(mode, key, result)
 end
 
 local custom_attach = function(client)
-	local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
-  wk.register({ ["<leader>l"] = { name = 'LSP'}})
+  local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
+  wk.register({["<leader>l"] = {name = 'LSP'}})
 
-	mapper('n', 'gd', 'lua vim.lsp.buf.definition()')
-	mapper('n', 'gD', 'lua vim.lsp.buf.declaration()')
-	-- mapper('n', 'gr', 'lua vim.lsp.buf.references()')
-	mapper('n', 'gr', 'Telescope lsp_references')
-	mapper('n', 'gi', 'lua vim.lsp.buf.implementation()')
+  mapper('n', 'gd', 'lua vim.lsp.buf.definition()')
+  mapper('n', 'gD', 'lua vim.lsp.buf.declaration()')
+  -- mapper('n', 'gr', 'lua vim.lsp.buf.references()')
+  mapper('n', 'gr', 'Telescope lsp_references')
+  mapper('n', 'gi', 'lua vim.lsp.buf.implementation()')
 
-	mapper('n', '[d', mappings.diagnostic_prev)
-	mapper('n', ']d', mappings.diagnostic_next)
-	mapper('n', '<leader>dp', mappings.diagnostic_prev)
-	mapper('n', '<leader>dn', mappings.diagnostic_next)
-	mapper('n', '<leader>dl', mappings.diagnostic_line)
+  mapper('n', '[d', mappings.diagnostic_prev)
+  mapper('n', ']d', mappings.diagnostic_next)
+  mapper('n', '<leader>dp', mappings.diagnostic_prev)
+  mapper('n', '<leader>dn', mappings.diagnostic_next)
+  mapper('n', '<leader>dl', mappings.diagnostic_line)
 
-	-- mapper('n', '<leader>lf', 'lua vim.lsp.buf.formatting()')
-  wk.register({ ["<leader>lf"] = { '<cmd>lua vim.lsp.buf.formatting()<cr>', 'Format'}})
+  -- mapper('n', '<leader>lf', 'lua vim.lsp.buf.formatting()')
+  wk.register({["<leader>lf"] = {'<cmd>lua vim.lsp.buf.formatting()<cr>', 'Format'}})
 
-	if filetype ~= 'lua' then
-    mapper('n', 'K', mappings.hover)
-  end
+  if filetype ~= 'lua' then mapper('n', 'K', mappings.hover) end
 
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
@@ -71,32 +60,15 @@ local custom_attach = function(client)
 
 end
 
-
-
 -- override defaults
-lspconfig.util.default_config = vim.tbl_extend(
-  "force",
-  lspconfig.util.default_config,
-  {
-    on_attach = custom_attach
-  }
-)
+lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {on_attach = custom_attach})
 
 require('cc/lsp/efm')
 require('cc/lsp/lua')
 
 -- Install server with:
 -- npm install -g typescript typescript-language-server
-lspconfig.tsserver.setup{}
+lspconfig.tsserver.setup {}
 
-
-lspconfig.yamlls.setup({
-  settings = {
-    yaml = {
-      schemas = {
-        kubernetes = "/*.yaml"
-      }
-    }
-  }
-})
+lspconfig.yamlls.setup({settings = {yaml = {schemas = {kubernetes = "/*.yaml"}}}})
 
--- lua/cc/which-key.lua
+++ lua/cc/which-key.lua
@@ -2,29 +2,28 @@ local wk = require("which-key")
 
 wk.register({
   ["<leader>"] = {
-    b = { name = "Buffers" },
-    d = { name = "Diagnostics" },
+    b = {name = "Buffers"},
+    d = {name = "Diagnostics"},
     f = {
       name = "File",
-			o = { 'Show in file tree'},
-      f = { "Open File" },
-      r = { "Open Recent File" },
+      o = {'Show in file tree'},
+      f = {"Open File"},
+      r = {"Open Recent File"},
       t = 'toggle file tree'
       -- n = { "<cmd>enew<cr>", "New File" },
     },
-		g = { name = "Git"},
-		h = { name = "Help"},
-    o = { name = "Org/Wiki"},
-		s = { name = "Search"},
-		t = { name = "Toggle",
-			h = "highlight"
-	  },
-		w = { name = "Windows",
+    g = {name = "Git"},
+    h = {name = "Help"},
+    o = {name = "Org/Wiki"},
+    s = {name = "Search"},
+    t = {name = "Toggle", h = "highlight"},
+    w = {
+      name = "Windows",
       o = 'Focus current window',
       s = 'Horizontal Split',
       v = 'Vertical Split',
       w = 'Cycle'
-    },
-  },
+    }
+  }
 })
 
