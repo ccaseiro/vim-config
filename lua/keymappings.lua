140328554081e7338f2ec8a10886506e117ad2a9
Claudio Caseiro <ccaseiro@gmail.com>
2021-06-28 20:53:51 +0100

    fix: if map on all modes, conflicts with vsnippet

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -27,8 +27,8 @@ vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silen
 vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
 
 -- Plugin: nvim-miniyank
-vim.api.nvim_set_keymap('', 'p', '<Plug>(miniyank-autoput)', {noremap = false, silent = true})
-vim.api.nvim_set_keymap('', 'P', '<Plug>(miniyank-autoPut)', {noremap = false, silent = true})
+vim.api.nvim_set_keymap('n', 'p', '<Plug>(miniyank-autoput)', {noremap = false, silent = true})
+vim.api.nvim_set_keymap('n', 'P', '<Plug>(miniyank-autoPut)', {noremap = false, silent = true})
 
 -- TAB switch buffer
 vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
