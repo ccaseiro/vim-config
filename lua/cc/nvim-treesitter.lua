fb89e6c972dbe400ab51180acdd44d6236a92755
Claudio Caseiro <ccaseiro@gmail.com>
2021-06-29 20:16:20 +0100

    additional_vim_regex_highlighting ???

--- lua/cc/nvim-treesitter.lua
+++ lua/cc/nvim-treesitter.lua
@@ -2,6 +2,13 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   -- ignore_install = { "fortran" }, -- List of parsers to ignore installing
   highlight = {
-    enable = true -- false will disable the whole extension
+    enable = true, -- false will disable the whole extension
+    additional_vim_regex_highlighting = true
   },
+  incremental_selection = {
+    enable = true
+    -- keymaps = {init_selection = "gnn", node_incremental = "grn", scope_incremental = "grc", node_decremental = "grm"}
+  },
+  indent = {enable = true}
+
 }
