7392a77f7a0e8e542bb5ce677635a291dd2af685
Claudio Caseiro <ccaseiro@gmail.com>
2021-06-29 20:44:25 +0100

    migrate to vim.opt

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,18 +1,19 @@
-vim.wo.number = true
-vim.wo.relativenumber = true -- without 'number', current line is always '0' 
--- vim.wo.signcolumn = "auto:2-4" -- TODO: not working with telescope. Need to test/report
-vim.wo.signcolumn = "yes:2"
+local opt = vim.opt
 
-vim.o.hidden=true               -- required to keep multiple buffers open multiple buffers
-vim.o.clipboard = "unnamedplus"
-vim.o.cursorline = true
-vim.o.fileencoding = "utf-8"
-vim.o.mouse = "a" 		-- enable mouse
--- vim.o.guifont = "Dank\\ Mono:h11,Fira\\ Code:h12"
-vim.o.termguicolors = true
-vim.o.timeoutlen = 300
-vim.o.updatetime = 300 -- Faster CursorHold events
-vim.bo.undofile = true -- if I only use "o", then first buffer gets "noundofile"
-vim.o.undofile = true -- if I only put "bo", then only first buffer gets undo.
-vim.o.listchars = 'eol:↲,tab:▷-,space: ,trail:·,extends:☛,precedes:☚,conceal:┊,nbsp:☠'
-vim.wo.list = true
+opt.number = true
+opt.relativenumber = true -- without 'number', current line is always '0'
+-- opt.signcolumn = "auto:2-4" -- TODO: not working with telescope. Need to test/report
+opt.signcolumn = "yes:2"
+
+opt.hidden = true -- required to keep multiple buffers open multiple buffers
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+opt.fileencoding = "utf-8"
+opt.mouse = "a" -- enable mouse
+-- opt.guifont = "Dank\\ Mono:h11,Fira\\ Code:h12"
+opt.termguicolors = true
+opt.timeoutlen = 300
+opt.updatetime = 300 -- Faster CursorHold events
+opt.undofile = true
+opt.listchars = 'eol:↲,tab:▷-,space: ,trail:·,extends:☛,precedes:☚,conceal:┊,nbsp:☠'
+opt.list = true
