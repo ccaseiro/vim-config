c35be311943ba44661a40ddb6b5904a7aa5311da
Claudio Caseiro <ccaseiro@gmail.com>
2021-06-30 16:03:39 +0100

    set *.js as javascriptreact filetype (because of snipets)

--- lua/cc/autocommands.lua
+++ lua/cc/autocommands.lua
@@ -1,7 +1,10 @@
 local utils = require('cc/utils')
 
 utils.define_augroups({
-    _general_settings = {
-        {'TextYankPost', '*', 'lua require(\'vim.highlight\').on_yank({higroup = \'Search\', timeout = 200})'}
-    }
+  _general_settings = {
+    {'TextYankPost', '*', 'lua require(\'vim.highlight\').on_yank({higroup = \'Search\', timeout = 200})'},
+    -- I'm using BufNewFile,BufRead instead of BufEnter, because with BufEnter I can't ovewride the option
+    -- with modeline (ex. // vim: set ft=javascript:)
+    {'BufNewFile,BufRead', '*.js', 'setlocal filetype=javascriptreact'}
+  }
 })
