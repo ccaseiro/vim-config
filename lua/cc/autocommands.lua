a9178298c8533946c1362532cf85ff21a281df97
Claudio Caseiro <ccaseiro@gmail.com>
2021-07-02 16:48:08 +0100

    fix coment about modeline

--- lua/cc/autocommands.lua
+++ lua/cc/autocommands.lua
@@ -4,7 +4,8 @@ utils.define_augroups({
   _general_settings = {
     {'TextYankPost', '*', 'lua require(\'vim.highlight\').on_yank({higroup = \'Search\', timeout = 200})'},
     -- I'm using BufNewFile,BufRead instead of BufEnter, because with BufEnter I can't ovewride the option
-    -- with modeline (ex. // vim: set ft=javascript:)
+    -- with modeline (ex. //vim: set ft=javascript:)
+    -- The previous line needs a space after `//`, but I can't do it here otherwise it executes the modeline.
     {'BufNewFile,BufRead', '*.js', 'setlocal filetype=javascriptreact'}
   }
 })
