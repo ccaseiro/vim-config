1ef22248ecdf83af923c936092bb0980e8923e98
Claudio Caseiro <ccaseiro@gmail.com>
2021-07-08 16:49:24 +0100

    -

--- lua/cc/init.lua
+++ lua/cc/init.lua
@@ -1,10 +1,9 @@
 require'colorizer'.setup()
-require'cc.gitsigns'
+require 'cc.gitsigns'
 require('nvim-autopairs').setup()
-require'cc.undotree'
-require'cc.nvim-lightbulb'
-require'cc.vista'
+require 'cc.undotree'
+require 'cc.nvim-lightbulb'
+require 'cc.vista'
 
 -- Startify
-vim.api.nvim_set_keymap('n', '<leader>bh', ':Startify<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<leader>bh', ':Startify<CR>', {noremap = true, silent = true})
