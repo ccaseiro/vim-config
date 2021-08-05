0f2221af933c93c40de254a682b11a2beb430c66
Claudio Caseiro <ccaseiro@gmail.com>
2021-08-05 02:59:10 +0100

    tab setup

--- lua/cc/bufferline.lua
+++ lua/cc/bufferline.lua
@@ -1,4 +1,3 @@
 require'bufferline'.setup()
-vim.api.nvim_set_keymap('n', '<TAB>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<F11>', ':BufferLineCycleNext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F10>', ':BufferLineCyclePrev<CR>', {noremap = true, silent = true})
--- lua/cc/compe.lua
+++ lua/cc/compe.lua
@@ -71,9 +71,9 @@ _G.s_tab_complete = function()
 end
 
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+-- vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+-- vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", {silent = true, expr = true})
 vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm('<CR>')", {silent = true, expr = true})
